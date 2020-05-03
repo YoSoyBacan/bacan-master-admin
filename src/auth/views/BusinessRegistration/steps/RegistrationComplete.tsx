556b30be0780c7126f22650085b1bee9dd7dc7b6
latinacodes9 <64444162+latinacodes9@users.noreply.github.com>
2020-05-03 00:21:11 -0500

    Terms and Conditions for Registration

--- src/auth/views/BusinessRegistration/steps/RegistrationComplete.tsx
+++ src/auth/views/BusinessRegistration/steps/RegistrationComplete.tsx
@@ -2,17 +2,15 @@ import { createStyles, Theme, WithStyles, withStyles } from '@material-ui/core/s
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 
-import Container from '../../../../components/Container';
-
 const styles = (theme: Theme) =>
   createStyles({
     title: {
+      
         flex: 1,
-        textAlign: "center",
-        paddingBottom: theme.spacing.unit * 2
+        textAlign: "center"
       },
-    svg: {
-      margin: 0
+    subtitle: {
+      marginBottom: "30px"
     }
   });
 
@@ -24,21 +22,36 @@ const RegistrationComplete = withStyles(styles, { name: "RegistrationComplete" }
   ({classes, businessLink}: RegistrationCompleteProps) => {
     return (
         <>
-        <Typography variant="h5" className={classes.title}>Gracias por registrar tu negocio y hacerlo más Bacán!</Typography>
-          <Container>
-            <Typography variant="subtitle2">
-              Quieres recibir más ingresos? Comparte este link <a href={businessLink}>{businessLink}</a> en tus redes sociales para que tus clientes apoyen tu negocio.
+        <Typography variant="h4" className={classes.title}>&#9989; Ya estas 100% registrado en Bacán &#9989; </Typography>
+            <Typography variant="subtitle1" className={classes.subtitle}>
+            <br/>
+              Qué viene ahora? 
                 <br/>
-                <br/> 1. Copia el Link <a href={businessLink}>{businessLink}</a> 
-                <br/> 2.Pegalo en tu Red Social
-                <br/> &emsp; a. En intagram pega el link en tu biografía
-                <br/> &emsp; b. Comparte el link en las descripciones de tus imágenes
-                <br/> &emsp; c. Comparte el link en tu Muro de Facebook
-                <br/> &emsp; d. Envia el link a tus contactos en Whatsapp
+                <br/> El lanzamiento de la tienda Bacán y tu administrador personal <a href="https://www.negocio.yosoybacan.com">negocio.yosoybacan.com</a> séra esta semána &#10024;
+                <br/> &#10071;Atento a tu email&#10071;
+                <br/><br/> En tu administrador podrás ver la informacíon de tus ventas, clientes, tarjetas y podrás hacer los cambios que quieras. 
+                <br/><br/> &#128526; Durante esta semana te enviaremos informacíon sobre:
+                <br/> &emsp; &emsp;cómo funcionan los pagos a tu cuenta bancaria de tus ventas
+                <br/> &emsp; &emsp;cómo podrás redimir las tarjetas de tus compradores
+                <br/><br/>Tambien te mandaremos un video explicativo de como funcionara todo. &#128249;
+                <br/> Hemos generado un &#128279; link unico &#128279; el cual podras compartir con tus clientes para que te encuentren en Bacán. 
+                <br/> Comparte este link cuando tu tienda este lista y puedas acceder a tu perfil&#10071;
                 <br/>
-                <br/> ¡Es muy fácil! Solo corre la voz y verás que más gente apoyará tu negocio.
+                <br/> 
+                &emsp;Tu link es: <strong>{businessLink}</strong>
+                <br/>
+                <br/> &#128076; Te recomendamos  
+                <br/>&emsp;&emsp;Pegar el link en tu biografía de Instragram 
+                <br/>&emsp;&emsp;Compartir el link en las descripciones de tus imágenes
+                <br/>&emsp;&emsp;Comparte el link en tu muro de Facebook
+                <br/>&emsp;&emsp;Envia el link a tus contactos en Whatsapp
+                <br/>
+                <br/>&#128165;¡Es muy fácil! Solo corre la voz y verás que más gente apoyará tu negocio! &#128165;
+                <br/>
+                <br/>
+                <br/>
+                - Bacán &#129321;
             </Typography>
-          </Container>
         </>
     );
   }
--- src/auth/views/BusinessRegistration/steps/UserAccount.tsx
+++ src/auth/views/BusinessRegistration/steps/UserAccount.tsx
@@ -5,6 +5,7 @@ import { createStyles, Theme, WithStyles, withStyles } from '@material-ui/core/s
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import CardSpacer from '@saleor/components/CardSpacer';
+import Checkbox from '@saleor/components/Checkbox';
 import ConfirmButton from '@saleor/components/ConfirmButton';
 import Container from '@saleor/components/Container';
 import Form from '@saleor/components/Form';
@@ -60,7 +61,14 @@ const styles = (theme: Theme) =>
     title: {
         flex: 1,
         paddingBottom: theme.spacing.unit * 2
-      },
+    },
+    checkboxContainer: {
+      display: 'flex',
+      paddingTop: '5%'
+    },
+    termsTitle: {
+      paddingTop: '1%'
+    },
     panel: {
       "& span": {
         color: theme.palette.error.contrastText
@@ -88,6 +96,7 @@ const UserAccount = withStyles(styles, { name: "UserAccount" })(
     const intl = useIntl();
     const notify = useNotifier();
     const [ loading, setLoading ] = React.useState(false);
+    const [ acceptedTerms, setTerms ] = React.useState(false);
     const [ errors, setErrors ] = React.useState<Array<{field: string, message: string}>>([])
 
     const setNewError = (field: string, message: string) => {
@@ -95,11 +104,21 @@ const UserAccount = withStyles(styles, { name: "UserAccount" })(
       newErrors.push({ field, message});
       setErrors(newErrors);
     }
+
+    const handleCheckBox = (checked) => {
+      if (checked === true){
+          setTerms(false);
+      } else {
+          setTerms(true);
+      }
+  }
+
     useEffect(() => {
       setTimeout(() => {
         document.querySelector("#content-panel").scrollTo({ top: 0, behavior: 'smooth'});
       }, 100);
     }, []);
+      
     const handleSubmit = async (data: FormData) => {
       setLoading(true);
       const values = Object.values(data);
@@ -120,6 +139,12 @@ const UserAccount = withStyles(styles, { name: "UserAccount" })(
         setLoading(false);
         return;
       } 
+
+      if (acceptedTerms != true){
+        notify({ text: 'Por favor acepte terminos y condiciones.' });
+        setLoading(false);
+        return;
+      }
 
       try{
         const newUser = await AdminClient.post<{ uid: string }>('user', {
@@ -292,6 +317,18 @@ const UserAccount = withStyles(styles, { name: "UserAccount" })(
                         "data-tc": "confirmPassword"
                     }}
                   />
+                  <FormSpacer />
+                  <div className={classes.checkboxContainer}>
+                    <Checkbox
+                      checked={acceptedTerms}
+                      disableClickPropagation
+                      onChange={() => handleCheckBox(acceptedTerms)}
+                    /> 
+                    <Typography variant="subtitle1" className={classes.termsTitle}>
+                        Acepto <a href="https://drive.google.com/file/d/1YXpxE5IAbwFyRKkVbhRWUOAYH5JJFGtM/view?usp=sharing" target="_blank"> Terminos y Condiciones </a>
+                    </Typography>
+                  </div>
+                  <FormSpacer />
                   <div className={classes.buttonContainer}>
                   <div/>
                     <ConfirmButton
